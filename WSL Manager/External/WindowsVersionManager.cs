64bb1390a35c3f0aa6c17047d15df8eacb1cac29
visdauas <visdauas@gmail.com>
2020-09-04 22:41:59 +0200

    Better Windows version management

--- WSL Manager/External/WindowsVersionManager.cs
+++ WSL Manager/External/WindowsVersionManager.cs
@@ -12,21 +12,7 @@ namespace WSL_Manager.External
         {
             int releaseId = int.Parse(Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion", "ReleaseId", "").ToString());
 
-            if(releaseId == WindowsVersion.V2009.Version)
-                CurrentVersion = WindowsVersion.V2009;
-            else if (releaseId == WindowsVersion.V2004.Version)
-                CurrentVersion = WindowsVersion.V2004;
-            else if (releaseId == WindowsVersion.V1909.Version)
-                CurrentVersion = WindowsVersion.V1909;
-            else if (releaseId == WindowsVersion.V1903.Version)
-                CurrentVersion = WindowsVersion.V1903;
-            else if (releaseId == WindowsVersion.V1809.Version)
-                CurrentVersion = WindowsVersion.V1809;
-            else if (releaseId == WindowsVersion.V1803.Version)
-                CurrentVersion = WindowsVersion.V1803;
-            else
-                CurrentVersion = new WindowsVersion(releaseId);
-
+            CurrentVersion = new WindowsVersion(releaseId);
 
             if (CurrentVersion.Version >= WindowsVersion.V1903.Version)
                 runningDistroCheckSupported = true;
